59ea95345c6c25de50c4419888135490bfa0a4be
Eddie Jaoude <eddie@jaoudestudios.com>
2012-07-11 06:58:55 +0100

    Added required Zend library (Loader, Reflection, Exception) & Sample code

---
+++ library/Sample/ClassWithFullDocBlocs.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Class with method that has full docbloc
+ */
+class Sample_ClassWithFullDocBlocs
+{
+
+    /**
+     * This is a sample method
+     *
+     * @param int $param1
+     * @param string $param2
+     */
+    public function sampleMethodWith2Parameters($param1, $param2)
+    {
+
+    }
+
+    /**
+     * This is a sample method2
+     *
+     * @param Zend_Config $param1
+     */
+    public function sampleMethod2With1Parameters(Zend_Config $param)
+    {
+
+    }
+
+}
---
+++ library/Sample/ClassWithNoDocBlocs.php
@@ -0,0 +1,11 @@
+<?php
+
+class Sample_ClassWithNoDocBlocs
+{
+
+    public function sampleMethodWith2Parameters($param1, $param2)
+    {
+
+    }
+
+}
