11041d1aa960b91fccb57945c6bcf73d1e8a292f
Eddie Jaoude <eddie@jaoudestudios.com>
2012-08-13 06:42:01 +0100

    Removed old Test library files

--- library/Test/File/Adapter/Conflict.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Conflict adapter
- */
-class Test_File_Adapter_Conflict extends Test_File_Adapter_Core
-{
-
-}
--- library/Test/File/Adapter/DebugCode.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Debug code adapter
- */
-class Test_File_Adapter_DebugCode extends Test_File_Adapter_Core
-{
-
-
-}
