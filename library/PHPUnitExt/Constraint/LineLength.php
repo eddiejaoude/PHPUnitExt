5c9a9780c0f3dbb504ac4b1a98b7d27aaa3f6210
Eddie Jaoude <eddie@jaoudestudios.com>
2012-08-15 07:51:37 +0100

    Improved unit tests & boundary conditions

--- library/PHPUnitExt/Constraint/LineLength.php
+++ library/PHPUnitExt/Constraint/LineLength.php
@@ -14,9 +14,16 @@ class PHPUnitExt_Constraint_LineLength implements PHPUnitExt_Constraint_Interfac
      *
      * @param array $data
      * @return bool
+     * @throws PHPUnitExt_Constraint_Exception
      */
     public function test($data)
     {
+        if (empty($data['line']) || empty($data['length'])) {
+            throw new PHPUnitExt_Constraint_Exception(
+                'Must contain $data[\'line\'] and $data[\'length\'] elements in $data array'
+            );
+        }
+
         if (strlen($data['line']) <= $data['length']) {
             return true;
         }
@@ -33,6 +40,12 @@ class PHPUnitExt_Constraint_LineLength implements PHPUnitExt_Constraint_Interfac
      */
     public function fail($data)
     {
+        if (empty($data['key']) || empty($data['file']) || empty($data['length'])) {
+            throw new PHPUnitExt_Constraint_Exception(
+                'Must contain $data[\'key\'], $data[\'file\'] and $data[\'length\'] elements in $data array'
+            );
+        }
+
         $data['key']++; // index starts from 0, but lines should be from 1
         $failure = 'Failed asserting file %s on line %d is less than %s';
         $failure = sprintf($failure, $data['file'], $data['key'], $data['length']);
---
+++ tests/library/PHPUnitExt/Constraint/LineLengthTest.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Line length constraint test
+ *
+ * @author Eddie Jaoude
+ * @package PHPUnitExt
+ */
+class PHPUnitExt_Constraint_LineLengthTest extends BaseTestCase
+{
+    /**
+     * Constraint object
+     *
+     * @var PHPUnitExt_Constraint_Interface
+     */
+    protected $_constraint;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->_constraint = new PHPUnitExt_Constraint_LineLength;
+    }
+
+    /**
+     * Tests constraint with invalid data
+     *
+     * @return bool
+     */
+    public function testTestInvalidData()
+    {
+        $data = array();
+        try {
+            $result = $this->_constraint->test($data);
+        } catch (PHPUnitExt_Constraint_Exception $e) {
+            $this->assertEquals(
+                'Must contain $data[\'line\'] and $data[\'length\'] elements in $data array',
+                $e->getMessage()
+            );
+            return true;
+        } catch (Exception $e) {
+            $this->fail('PHPUnitExt_Constraint_Exception Exception not thrown');
+        }
+        $this->fail('Exception not thrown');
+    }
+
+    /**
+     * Test constraint returns true
+     */
+    public function testTestReturnTrue()
+    {
+        $data = array(
+            'line' => 'abcdef',
+            'length' => 10
+        );
+        $result = $this->_constraint->test($data);
+        $this->assertEquals(true, $result);
+    }
+
+    /**
+     * Test constraint returns false
+     */
+    public function testTestReturnFalse()
+    {
+        $data = array(
+            'line' => 'abcdef',
+            'length' => 4
+        );
+        $result = $this->_constraint->test($data);
+        $this->assertEquals(false, $result);
+    }
+
+    /**
+     * Test fail method with invalid data
+     *
+     * @return bool
+     */
+    public function testFailInvalidData()
+    {
+        $data = array();
+        try {
+            $this->_constraint->fail($data);
+        } catch (PHPUnitExt_Constraint_Exception $e) {
+            $message = 'Must contain $data[\'key\'], $data[\'file\'] and $data[\'length\'] elements in $data array';
+            $this->assertEquals($e->getMessage(), $message);
+            return true;
+        } catch (Exception $e) {
+            $this->fail('PHPUnitExt_Constraint_Exception Exception not thrown');
+        }
+        $this->fail('Exception not thrown');
+    }
+
+    /**
+     * Test fail method
+     *
+     * @return bool
+     */
+    public function testFail()
+    {
+        $data = array(
+            'key' => 0,
+            'file' => CODE_PATH . '/ClassWithFullDocBlocs.php',
+            'length' => 10,
+        );
+        try {
+            $this->_constraint->fail($data);
+        } catch (PHPUnitExt_Constraint_Exception $e) {
+            $exception = 'Must contain $data[\'key\'], $data[\'file\'] and $data[\'length\'] elements in $data array';
+            $message = sprintf($exception, $data['file'], $data['key'], $data['length']);
+            $this->assertEquals($e->getMessage(), $message);
+            return true;
+        } catch (Exception $e) {
+            $this->fail('PHPUnitExt_Constraint_Exception Exception not thrown');
+        }
+        $this->fail('Exception not thrown');
+    }
+
+}
